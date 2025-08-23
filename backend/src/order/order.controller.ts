3af78e462e2631588f6978d239c969abd37bc332
Serhii Yallin <SerYallin@yandex.kz>
2025-08-23 19:42:21 +0300

    Add start components for NodeJs

---
+++ backend/src/order/order.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Post } from '@nestjs/common';
+import { OrderService } from './order.service';
+
+@Controller('order')
+export class OrderController {
+  constructor(private orderService: OrderService) {}
+  @Post('/')
+  createOrder() {
+    return {};
+  }
+}
